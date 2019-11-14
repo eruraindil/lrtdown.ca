64e9501dcc62e3ff03ecc359f8fa5a8ab2d10d85
Matthew Roberts <matt@mroberts.me>
2019-11-14 16:41:45 -0500

    some twitter work

---
+++ resources/views/index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ottawa's LRT Down?</title>
+
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+    <script defer src="{{ mix('/js/app.js') }}"></script>
+</head>
+<body>
+    @if ($lrtDown)
+    <section class="hero is-danger is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title is-1">
+                    Is the LRT Down? Yes
+                </h1>
+                <h2 class="subtitle is-3">
+                    0️⃣ 0️⃣ days issue free
+                <h2>
+            </div>
+        </div>
+    </section>
+    @else
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title is-1">
+                    Is the LRT Down? No (<a class="has-text-white" href="#">but maybe yes</a> ¯\_(ツ)_/¯)
+                </h1>
+                <h2 class="subtitle is-3">
+                    0️⃣ 1️⃣ days issue free
+                <h2>
+            </div>
+        </div>
+    </section>
+    @endif
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                <strong>LRTdown.ca</strong> by <a href="https://twitter.com/eruraindil">@eruraindil</a>. This is not an official website. Not even close.
+            </p>
+        </div>
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Abraham\TwitterOAuth\TwitterOAuth;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +14,54 @@
 */
 
 Route::get('/', function () {
-    return view('no');
+    $connection = new TwitterOAuth(
+        env('TWITTER_CONSUMER_KEY'),
+        env('TWITTER_CONSUMER_SECRET'),
+        env('TWITTER_ACCESS_TOKEN'),
+        env('TWITTER_ACCESS_SECRET')
+    );
+    $content = $connection->get('search/tweets', [
+        'q' => 'from:OCTranspoLive "Line 1"',
+        'count' => 1000,
+        'tweet_mode' => 'extended',
+        'result_type' => 'recent'
+    ]);
+    dump($content->statuses);
+
+    $tweets = array_map(function($t) {
+        return $t->full_text;
+    }, $content->statuses);
+    dump($tweets);
+
+    // $filtered_tweets = [];
+
+    // foreach ($content->statuses as $tweet) {
+    // }
+
+    $filteredTweets = preg_grep('/(delay|close)/miU', $tweets);
+
+    $filteredTweets = array_diff(
+        $filteredTweets,
+        preg_grep('/(minor|slight|small)\s\w?\s?(delay|delayed)/miU', $filteredTweets),
+        preg_grep('/(restore|complete|resume|open)/miU', $filteredTweets),
+        preg_grep('/(without|no)\s\w?\s?delay/miU', $filteredTweets)
+    );
+
+    dump($filteredTweets);
+
+    reset($filteredTweets);
+    $key = key($filteredTweets);
+    $lastTweet = $content->statuses[$key];
+
+    if (strtotime($lastTweet->created_at) > strtotime('-24 hours')) {
+        $lrtDown = true;
+    } else {
+        $lrtDown = false;
+    }
+
+    dump($lrtDown);
+
+    return view('index', compact('lrtDown'));
 });
 Route::get('/yes', function () {
     return view('yes');
