d9f3791365c43fb6ee4e0bafe92dd76e633ffd0c
Matthew Roberts <matt@mroberts.me>
2019-11-20 09:29:28 -0500

    Revert "use stderr for logging, per heroku suggestion"
    This reverts commit 62ead1dcebc73bf725679253e6445abd8a0f60a6.

--- config/logging.php
+++ config/logging.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stderr'),
+    'default' => env('LOG_CHANNEL', 'errorlog'),
 
     /*
     |--------------------------------------------------------------------------
