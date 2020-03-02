750704579462571884de3cb4df02793356a33cf7
Matthew Roberts <matt@mroberts.me>
2020-03-02 18:42:26 -0500

    regex update: longer wait times

--- app/Tweet.php
+++ app/Tweet.php
@@ -76,7 +76,7 @@ class Tweet extends Model
             Log::debug($tweet);
         }
 
-        $filteredTweets = preg_grep('/((delay|close)|(eastbound.*westbound|westbound.*eastbound)|(eastbound|westbound)\s?(platform)?\sonly|r1.*(service|between|operat|to|from)|allow extra travel time|experience (slightly\s)?longer travel time|longer waits|longer (wait and travel|travel and wait) time|switch issue|replacement bus|(door|brake) fault|stopped train|(must|should) use ((eastbound|westbound) )?platform|wait times of up to \d\d+ (minutes|mins)|reduced (train )?service|train shortage|shortage of trains|currently \d\d? trains|\d\d? trains (are\s)?in service|trains (are\s)?(being\s)?held|power issue|transfer between trains)/miU', $tweets);
+        $filteredTweets = preg_grep('/((delay|close)|(eastbound.*westbound|westbound.*eastbound)|(eastbound|westbound)\s?(platform)?\sonly|r1.*(service|between|operat|to|from)|allow extra travel time|experience (slightly\s)?longer travel time|longer waits|longer wait time|longer (wait and travel|travel and wait) time|switch issue|replacement bus|(door|brake) fault|stopped train|(must|should) use ((eastbound|westbound) )?platform|wait times of up to \d\d+ (minutes|mins)|reduced (train )?service|train shortage|shortage of trains|currently \d\d? trains|\d\d? trains (are\s)?in service|trains (are\s)?(being\s)?held|power issue|transfer between trains)/miU', $tweets);
 
         // (special|s1) bus(es| service) // removing S1 service from trigger phrases. https://twitter.com/LRTdown/status/1224699103646572544
 
